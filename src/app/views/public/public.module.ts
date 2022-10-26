c07bcd94d9ba3eda8a1a3ceee17ea604de0b9e24
tmate93 <tr.mate93@gmail.com>
2022-10-26 20:39:01 +0200

    generate registration component

--- src/app/views/public/public.module.ts
+++ src/app/views/public/public.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegistrationComponent
+  ],
   imports: [
     CommonModule
   ]
