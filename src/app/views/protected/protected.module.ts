dd34a34a4683dc3cf7aec6dce6b0986ab0d64805
tmate93 <tr.mate93@gmail.com>
2022-10-26 20:41:32 +0200

    generate file-upload component

--- src/app/views/protected/protected.module.ts
+++ src/app/views/protected/protected.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GalleryComponent } from './gallery/gallery.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
 
 @NgModule({
   declarations: [
-    GalleryComponent
+    GalleryComponent,
+    FileUploadComponent
   ],
   imports: [
     CommonModule
