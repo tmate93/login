4d36da6a072fb5ea1055338a9bfdc52f3cfe6365
tmate93 <tr.mate93@gmail.com>
2022-10-26 21:18:54 +0200

    update login component

--- src/app/views/public/login/login.component.ts
+++ src/app/views/public/login/login.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
   constructor() { }
 
-  ngOnInit(): void {
+  loginUser(): void{
+    if (!this.loginForm.valid) {
+      return;
+    }
+    const loginEmail = this.loginForm.value.email;
+    const loginPassword = this.loginForm.value.password;
   }
 
 }
