ae9ec2191f5997f58b10511316986ffe5b6bdb25
tmate93 <tr.mate93@gmail.com>
2022-10-27 17:57:52 +0200

    update design of gallery component

--- src/app/views/protected/gallery/gallery.component.ts
+++ src/app/views/protected/gallery/gallery.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FileData } from "../../../core/models/file/file.model";
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent {
 
-  constructor() { }
+  testFiles: FileData[];
 
-  ngOnInit(): void {
+  constructor() {
+    this.testFiles = [
+      {
+        fileName: 'testFile',
+        fileSize: 1234,
+        fileData: 'data',
+        uploadDate: new Date()
+      },
+      {
+        fileName: 'testFile2',
+        fileSize: 1234,
+        fileData: 'data',
+        uploadDate: new Date()
+      },
+      {
+        fileName: 'testFile3',
+        fileSize: 1234,
+        fileData: 'data',
+        uploadDate: new Date()
+      }
+    ];
   }
-
 }
+
