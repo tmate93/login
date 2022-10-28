0da7a7413fa3fae1f4853c18dad2cacf0f05956d
tmate93 <tr.mate93@gmail.com>
2022-10-28 13:15:20 +0200

    create unit test for register function

--- src/app/core/services/auth-service/auth.service.spec.ts
+++ src/app/core/services/auth-service/auth.service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { User } from "../../models/user/user.model";
+import { Router } from "@angular/router";
 
 describe('AuthServiceService', () => {
   let service: AuthService;
+  let mockUser: User = {
+    firstName: 'First',
+    lastName: 'Last',
+    email: 'email@provider.com',
+    password: '12345'
+  };
+  let mockRouter = {
+    navigate: () => {}
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Router, useValue: mockRouter },
+      ]
+    });
     service = TestBed.inject(AuthService);
+    localStorage.clear();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should register user to localStorage', () => {
+    service.register(mockUser);
+    let users = JSON.parse(<string>localStorage.getItem('users'));
+    expect(users.find((user: any) => mockUser.email === user.email && mockUser.password === user.password)).toBeTruthy();
   });
 });
